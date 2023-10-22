556142e4d9d3108acc34904437eb525ed21ee977
Luis Chang <luis.chang@outlook.com>
2023-10-22 17:40:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/semana09dpafirebase/LoginActivity.kt
@@ -0,0 +1,45 @@
+package com.example.semana09dpafirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        var auth = FirebaseAuth.getInstance()
+
+        val txtEmail: EditText = findViewById(R.id.txtEmail)
+        val txtPassword: EditText = findViewById(R.id.txtPassword)
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        val btnRegister: Button = findViewById(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            val correo = txtEmail.text.toString()
+            val clave = txtPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(correo,clave)
+                .addOnCompleteListener(this) {task ->
+                    if(task.isSuccessful){
+                        Snackbar
+                            .make(findViewById(android.R.id.content),
+                                "Ingreso exitoso",
+                                Snackbar.LENGTH_LONG).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }else{
+                        Snackbar
+                            .make(findViewById(android.R.id.content),
+                                "Credenciales inválidas",
+                                Snackbar.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+    }
+}
